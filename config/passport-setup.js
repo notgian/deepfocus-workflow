10fcc64099711e0c0a7266a82d18ab4b6316e8e3
ross <197703104+kenneseRoss236@users.noreply.github.com>
2026-05-05 18:57:12 +0800

    Add Passport serialize/deserialize handlers

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -40,3 +40,15 @@ passport.use(new GoogleStrategy({
     });
 
 }));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    }).catch((err) => {
+        done(err, null);
+    });
+});
