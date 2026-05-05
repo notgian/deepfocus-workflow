084321ad3ddfd6811de281bb0c82e6a933b5545c
ross <197703104+kenneseRoss236@users.noreply.github.com>
2026-05-05 18:46:30 +0800

    create authRoute,js

---
+++ routes/authRoute.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+// auth login
+router.get('/login', (req, res) => {
+    // handle with passport 
+    res.send('Login route');
+});
+
+// auth logout
+router.get('/logout', (req, res) => {
+    // handle with passport 
+    res.send('Logout route');
+});
+
+// auth with google
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+// callback route for google to redirect to
+router.get('/google/callback', passport.authenticate('google'), (req, res) => {
+    
+    res.send('Google callback route');
+});
+
+module.exports = router;
