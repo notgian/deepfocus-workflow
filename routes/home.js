a2ddd0925da7b2dad535d4791bf61308c0f9a9a5
Gian <85060753+notgian@users.noreply.github.com>
2026-05-05 19:39:34 +0800

    used an animated moving gradient instead of waves to not give us a headache

--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,7 @@ const homeRoute = express.Router();
 
 homeRoute.get('/', (req, res) => {
     res.render('index.hbs', {
+        title: 'Deepfocus Workflow',
         css: ['/css/home.css']
     })
 });
