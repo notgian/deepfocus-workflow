94b508a55e4a46cb1b5e90f775dd6ab6f520f48c
Gian <85060753+notgian@users.noreply.github.com>
2026-05-06 17:36:41 +0800

    projects are displayed dynamically from db data

--- routes/auth.js
+++ routes/auth.js
@@ -21,9 +21,23 @@ router.get('/google', passport.authenticate('google', {
 
 // callback route for google to redirect to
 router.get('/google/callback', passport.authenticate('google'), (req, res) => {
-    
+
     res.send('Google callback route');
 });
+
+// TODO: remove this
+// temporary route to set the user session
+router.get('/fakelogin', (req, res) => {
+    req.session.user = {
+        _id: "69fad1c5e9591cf51e1b0d5b",
+        googleId: "109259785027979911517",
+        displayName: "Gian Ortha",
+        email: "gianlorenzoortha@gmail.com",
+        displayImage: "https://lh3.googleusercontent.com/a/ACg8ocJlXzSv-o8z3iqY01wiD_eo9FbT1NLWJsW2t-G4j70oJxQNug=s96-c"
+    }
+
+    res.send(`Fake user login success! ${JSON.stringify(req.session.user)}`)
+})
 
 module.exports = {
     authRouter: router
--- routes/projects.js
+++ routes/projects.js
@@ -1,12 +1,25 @@
 
 const express = require('express');
 
+const Projects = require('../models/projects.js');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
+// TODO require google passport authentication
+router.get('/', async (req, res) => {
+    if (!req.session.user)
+        return res.redirect('/auth/fakelogin')
+    
+    // get user projects
+    const projects = await Projects.find({
+        userId: req.session.user._id 
+    }).lean();
+
     res.render('projects.hbs', {
         title: 'Projects | Deepfocus Workflow',
-        css: ['/css/projects.css']
+        css: ['/css/projects.css'],
+        user: req.session.user,
+        projects: projects
     })
 });
 
