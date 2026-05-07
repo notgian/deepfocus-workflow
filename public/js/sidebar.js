a9cddf0896794f13df8dd9411b782cf7fea608a0
Gian <85060753+notgian@users.noreply.github.com>
2026-05-07 22:08:01 +0800

    magic now the sidebar looks fine in mobile view

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,9 +1,12 @@
 const sidebar = document.getElementById("sidebar");
+const mainContent = document.querySelector('.main-content');
 
 function closeSidebar() {
     sidebar.style.marginLeft = '-320px';
+    mainContent.style.marginLeft = '0px';
 }
 
 function openSidebar() {
     sidebar.style.marginLeft = '0px';
+    mainContent.style.marginLeft = '320px';
 }
