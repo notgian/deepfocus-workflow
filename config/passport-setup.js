52844d0de9a4e717408bd76eb57915b6b6618ccf
Gian <85060753+notgian@users.noreply.github.com>
2026-05-07 23:52:17 +0800

    changed callback url to deployment url

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -6,7 +6,7 @@ const Token = require('../models/tokens');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "https://deepfocus-workflow.vercel.app/auth/google/callback",
     passReqToCallback: true,
 }, async (req, accessToken, refreshToken, profile, done) => {
     try {
